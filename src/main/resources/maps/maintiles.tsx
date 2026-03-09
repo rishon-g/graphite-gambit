31585f431a910350d3fe60ac52d6079a7495bbd5
Rishon Ghosh <rishonghosh@Rishons-MacBook-Air-2498.local>
2026-03-09 15:38:07 -0700

    bug fixes

--- src/main/resources/maps/maintiles.tsx
+++ src/main/resources/maps/maintiles.tsx
@@ -1,35 +1,29 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="maintiles" tilewidth="1168" tileheight="1024" tilecount="23" columns="0">
+<tileset version="1.10" tiledversion="1.11.2" name="maintiles" tilewidth="1168" tileheight="1024" tilecount="22" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="73">
   <image source="../images/desk.png" width="310" height="296"/>
  </tile>
  <tile id="74">
-  <image source="../images/PlayStation 3 - LittleBigPlanet 3 - Cardboard - Torn Paper.png" width="1168" height="552"/>
+  <image source="../images/designset1.png" width="1168" height="552"/>
  </tile>
  <tile id="76">
-  <image source="../images/Screenshot 2026-03-09 at 1.21.11 PM.png" width="152" height="150"/>
- </tile>
- <tile id="77">
-  <image source="../images/image-removebg-preview.png" width="270" height="270"/>
- </tile>
- <tile id="78">
-  <image source="../images/PlayStation Portable - LittleBigPlanet - Cardboard - Blue Graph Paper (1).png" width="152" height="150"/>
+  <image source="../images/paper1.png" width="152" height="150"/>
  </tile>
  <tile id="79">
-  <image source="../images/PlayStation Portable - LittleBigPlanet - Cardboard - Blue Graph Paper.png" width="128" height="128"/>
+  <image source="../images/maptilepiece.png" width="128" height="128"/>
  </tile>
  <tile id="80">
-  <image source="../images/PlayStation Portable - LittleBigPlanet - Cardboard - Blue Graph Paper copy.png" width="128" height="128"/>
+  <image source="../images/cornermapthin.png" width="128" height="128"/>
  </tile>
  <tile id="81">
-  <image source="../images/PlayStation Portable - LittleBigPlanet - Cardboard - Blue Graph Paper copy 2.png" width="128" height="128"/>
+  <image source="../images/cornermap.png" width="128" height="128"/>
  </tile>
  <tile id="82">
-  <image source="../images/PlayStation Portable - LittleBigPlanet - Cardboard - Blue Graph Paper copy 3.png" width="128" height="128"/>
+  <image source="../images/sidemap.png" width="128" height="128"/>
  </tile>
  <tile id="83">
-  <image source="../images/image-removebg-preview (1).png" width="256" height="256"/>
+  <image source="../images/hole1.png" width="256" height="256"/>
  </tile>
  <tile id="84">
   <image source="../images/hole2.png" width="512" height="512"/>
@@ -44,13 +38,13 @@
   <image source="../images/scissors.png" width="256" height="256"/>
  </tile>
  <tile id="88">
-  <image source="../images/Classroom Props Second Spritesheet 4 (2).png" width="256" height="256"/>
+  <image source="../images/redpencil.png" width="256" height="256"/>
  </tile>
  <tile id="89">
-  <image source="../images/Classroom Props Second Spritesheet 4-2.png" width="256" height="256"/>
+  <image source="../images/cirlcedrawer.png" width="256" height="256"/>
  </tile>
  <tile id="90">
-  <image source="../images/Classroom Props Second Spritesheet 4.png" width="256" height="256"/>
+  <image source="../images/bluecrayon.png" width="256" height="256"/>
  </tile>
  <tile id="91">
   <image source="../images/hand-drawn-smiley-face-icon-with-positive-emotion-and-simple-design-isolated-on-transparent-background-png.png" width="640" height="640"/>
@@ -59,16 +53,19 @@
   <image source="../images/maxresdefault-removebg-preview.png" width="666" height="375"/>
  </tile>
  <tile id="93">
-  <image source="../images/images-removebg-preview (1) (1).png" width="768" height="768"/>
+  <image source="../images/rosesfit.png" width="768" height="768"/>
  </tile>
  <tile id="94">
-  <image source="../images/Gemini_Generated_Image_9gcs1x9gcs1x9gcs__1_-removebg-preview.png" width="707" height="353"/>
+  <image source="../images/sfudrawing.png" width="707" height="353"/>
  </tile>
  <tile id="95">
-  <image source="../images/unnamed-removebg-preview.png" width="677" height="369"/>
+  <image source="../images/mathequation1.png" width="677" height="369"/>
  </tile>
  <tile id="96">
   <image source="../images/5ad5e23f3a4b43531c523754f25ce4-removebg-preview.png" width="400" height="540"/>
+ </tile>
+ <tile id="97">
+  <image source="../images/1_ePZmG6fJPlBkKZHiNIdFsA-removebg-preview.png" width="528" height="473"/>
  </tile>
  <wangsets>
   <wangset name="Unnamed Set" type="corner" tile="-1">
