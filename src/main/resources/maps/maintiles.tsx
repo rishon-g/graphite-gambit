c304c17de75da306e1cddb3cc354bce7549e8921
Rishon Ghosh <rishonghosh@Rishons-MacBook-Air-2498.local>
2026-03-09 15:10:58 -0700

    updated much nicer map

--- src/main/resources/maps/maintiles.tsx
+++ src/main/resources/maps/maintiles.tsx
@@ -1,223 +1,78 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="maintiles" tilewidth="310" tileheight="296" tilecount="73" columns="0" tilerendersize="grid">
+<tileset version="1.10" tiledversion="1.11.2" name="maintiles" tilewidth="1168" tileheight="1024" tilecount="23" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image source="../images/23le000.png" width="152" height="150"/>
- </tile>
- <tile id="1">
-  <image source="../images/23le001.png" width="152" height="150"/>
- </tile>
- <tile id="2">
-  <image source="../images/23le002.png" width="152" height="150"/>
- </tile>
- <tile id="3">
-  <image source="../images/23le003.png" width="152" height="150"/>
- </tile>
- <tile id="4">
-  <image source="../images/23le004.png" width="152" height="150"/>
- </tile>
- <tile id="5">
-  <image source="../images/23le005.png" width="152" height="150"/>
- </tile>
- <tile id="6">
-  <image source="../images/23le006.png" width="152" height="150"/>
- </tile>
- <tile id="7">
-  <image source="../images/23le007.png" width="152" height="150"/>
- </tile>
- <tile id="8">
-  <image source="../images/23le008.png" width="152" height="150"/>
- </tile>
- <tile id="9">
-  <image source="../images/23le009.png" width="152" height="150"/>
- </tile>
- <tile id="10">
-  <image source="../images/23le010.png" width="152" height="150"/>
- </tile>
- <tile id="11">
-  <image source="../images/23le011.png" width="152" height="150"/>
- </tile>
- <tile id="12">
-  <image source="../images/23le012.png" width="152" height="150"/>
- </tile>
- <tile id="13">
-  <image source="../images/23le013.png" width="152" height="150"/>
- </tile>
- <tile id="14">
-  <image source="../images/23le014.png" width="152" height="150"/>
- </tile>
- <tile id="15">
-  <image source="../images/23le015.png" width="152" height="150"/>
- </tile>
- <tile id="16">
-  <image source="../images/23le016.png" width="152" height="150"/>
- </tile>
- <tile id="17">
-  <image source="../images/23le017.png" width="152" height="150"/>
- </tile>
- <tile id="18">
-  <image source="../images/23le018.png" width="152" height="150"/>
- </tile>
- <tile id="19">
-  <image source="../images/23le019.png" width="152" height="150"/>
- </tile>
- <tile id="20">
-  <image source="../images/23le020.png" width="152" height="150"/>
- </tile>
- <tile id="21">
-  <image source="../images/23le021.png" width="152" height="150"/>
- </tile>
- <tile id="22">
-  <image source="../images/23le022.png" width="152" height="150"/>
- </tile>
- <tile id="23">
-  <image source="../images/23le023.png" width="152" height="150"/>
- </tile>
- <tile id="24">
-  <image source="../images/23le024.png" width="152" height="150"/>
- </tile>
- <tile id="25">
-  <image source="../images/23le025.png" width="152" height="150"/>
- </tile>
- <tile id="26">
-  <image source="../images/23le026.png" width="152" height="150"/>
- </tile>
- <tile id="27">
-  <image source="../images/23le027.png" width="152" height="150"/>
- </tile>
- <tile id="28">
-  <image source="../images/23le028.png" width="152" height="150"/>
- </tile>
- <tile id="29">
-  <image source="../images/23le029.png" width="152" height="150"/>
- </tile>
- <tile id="30">
-  <image source="../images/23le030.png" width="152" height="150"/>
- </tile>
- <tile id="31">
-  <image source="../images/23le031.png" width="152" height="150"/>
- </tile>
- <tile id="32">
-  <image source="../images/23le032.png" width="152" height="150"/>
- </tile>
- <tile id="33">
-  <image source="../images/23le033.png" width="152" height="150"/>
- </tile>
- <tile id="34">
-  <image source="../images/232034.png" width="152" height="150"/>
- </tile>
- <tile id="35">
-  <image source="../images/232035.png" width="152" height="150"/>
- </tile>
- <tile id="36">
-  <image source="../images/tile000.png" width="151" height="150"/>
- </tile>
- <tile id="37">
-  <image source="../images/tile001.png" width="151" height="150"/>
- </tile>
- <tile id="38">
-  <image source="../images/tile002.png" width="151" height="150"/>
- </tile>
- <tile id="39">
-  <image source="../images/tile003.png" width="151" height="150"/>
- </tile>
- <tile id="40">
-  <image source="../images/tile004.png" width="151" height="150"/>
- </tile>
- <tile id="41">
-  <image source="../images/tile005.png" width="151" height="150"/>
- </tile>
- <tile id="42">
-  <image source="../images/tile006.png" width="151" height="150"/>
- </tile>
- <tile id="43">
-  <image source="../images/tile007.png" width="151" height="150"/>
- </tile>
- <tile id="44">
-  <image source="../images/tile008.png" width="151" height="150"/>
- </tile>
- <tile id="45">
-  <image source="../images/tile009.png" width="151" height="150"/>
- </tile>
- <tile id="46">
-  <image source="../images/tile010.png" width="151" height="150"/>
- </tile>
- <tile id="47">
-  <image source="../images/tile011.png" width="151" height="150"/>
- </tile>
- <tile id="48">
-  <image source="../images/tile012.png" width="151" height="150"/>
- </tile>
- <tile id="49">
-  <image source="../images/tile013.png" width="151" height="150"/>
- </tile>
- <tile id="50">
-  <image source="../images/tile014.png" width="151" height="150"/>
- </tile>
- <tile id="51">
-  <image source="../images/tile015.png" width="151" height="150"/>
- </tile>
- <tile id="52">
-  <image source="../images/tile016.png" width="151" height="150"/>
- </tile>
- <tile id="53">
-  <image source="../images/tile017.png" width="151" height="150"/>
- </tile>
- <tile id="54">
-  <image source="../images/tile018.png" width="151" height="150"/>
- </tile>
- <tile id="55">
-  <image source="../images/tile019.png" width="151" height="150"/>
- </tile>
- <tile id="56">
-  <image source="../images/tile020.png" width="151" height="150"/>
- </tile>
- <tile id="57">
-  <image source="../images/tile021.png" width="151" height="150"/>
- </tile>
- <tile id="58">
-  <image source="../images/tile022.png" width="151" height="150"/>
- </tile>
- <tile id="59">
-  <image source="../images/tile023.png" width="151" height="150"/>
- </tile>
- <tile id="60">
-  <image source="../images/tile024.png" width="151" height="150"/>
- </tile>
- <tile id="61">
-  <image source="../images/tile025.png" width="151" height="150"/>
- </tile>
- <tile id="62">
-  <image source="../images/tile026.png" width="151" height="150"/>
- </tile>
- <tile id="63">
-  <image source="../images/tile027.png" width="151" height="150"/>
- </tile>
- <tile id="64">
-  <image source="../images/tile028.png" width="151" height="150"/>
- </tile>
- <tile id="65">
-  <image source="../images/tile029.png" width="151" height="150"/>
- </tile>
- <tile id="66">
-  <image source="../images/tile030.png" width="151" height="150"/>
- </tile>
- <tile id="67">
-  <image source="../images/tile031.png" width="151" height="150"/>
- </tile>
- <tile id="68">
-  <image source="../images/tile032.png" width="151" height="150"/>
- </tile>
- <tile id="69">
-  <image source="../images/tile033.png" width="151" height="150"/>
- </tile>
- <tile id="70">
-  <image source="../images/tile034.png" width="151" height="150"/>
- </tile>
- <tile id="71">
-  <image source="../images/tile035.png" width="151" height="150"/>
- </tile>
  <tile id="73">
   <image source="../images/desk.png" width="310" height="296"/>
  </tile>
+ <tile id="74">
+  <image source="../images/PlayStation 3 - LittleBigPlanet 3 - Cardboard - Torn Paper.png" width="1168" height="552"/>
+ </tile>
+ <tile id="76">
+  <image source="../images/Screenshot 2026-03-09 at 1.21.11 PM.png" width="152" height="150"/>
+ </tile>
+ <tile id="77">
+  <image source="../images/image-removebg-preview.png" width="270" height="270"/>
+ </tile>
+ <tile id="78">
+  <image source="../images/PlayStation Portable - LittleBigPlanet - Cardboard - Blue Graph Paper (1).png" width="152" height="150"/>
+ </tile>
+ <tile id="79">
+  <image source="../images/PlayStation Portable - LittleBigPlanet - Cardboard - Blue Graph Paper.png" width="128" height="128"/>
+ </tile>
+ <tile id="80">
+  <image source="../images/PlayStation Portable - LittleBigPlanet - Cardboard - Blue Graph Paper copy.png" width="128" height="128"/>
+ </tile>
+ <tile id="81">
+  <image source="../images/PlayStation Portable - LittleBigPlanet - Cardboard - Blue Graph Paper copy 2.png" width="128" height="128"/>
+ </tile>
+ <tile id="82">
+  <image source="../images/PlayStation Portable - LittleBigPlanet - Cardboard - Blue Graph Paper copy 3.png" width="128" height="128"/>
+ </tile>
+ <tile id="83">
+  <image source="../images/image-removebg-preview (1).png" width="256" height="256"/>
+ </tile>
+ <tile id="84">
+  <image source="../images/hole2.png" width="512" height="512"/>
+ </tile>
+ <tile id="85">
+  <image source="../images/hole3.png" width="768" height="768"/>
+ </tile>
+ <tile id="86">
+  <image source="../images/hole4.png" width="1024" height="1024"/>
+ </tile>
+ <tile id="87">
+  <image source="../images/scissors.png" width="256" height="256"/>
+ </tile>
+ <tile id="88">
+  <image source="../images/Classroom Props Second Spritesheet 4 (2).png" width="256" height="256"/>
+ </tile>
+ <tile id="89">
+  <image source="../images/Classroom Props Second Spritesheet 4-2.png" width="256" height="256"/>
+ </tile>
+ <tile id="90">
+  <image source="../images/Classroom Props Second Spritesheet 4.png" width="256" height="256"/>
+ </tile>
+ <tile id="91">
+  <image source="../images/hand-drawn-smiley-face-icon-with-positive-emotion-and-simple-design-isolated-on-transparent-background-png.png" width="640" height="640"/>
+ </tile>
+ <tile id="92">
+  <image source="../images/maxresdefault-removebg-preview.png" width="666" height="375"/>
+ </tile>
+ <tile id="93">
+  <image source="../images/images-removebg-preview (1) (1).png" width="768" height="768"/>
+ </tile>
+ <tile id="94">
+  <image source="../images/Gemini_Generated_Image_9gcs1x9gcs1x9gcs__1_-removebg-preview.png" width="707" height="353"/>
+ </tile>
+ <tile id="95">
+  <image source="../images/unnamed-removebg-preview.png" width="677" height="369"/>
+ </tile>
+ <tile id="96">
+  <image source="../images/5ad5e23f3a4b43531c523754f25ce4-removebg-preview.png" width="400" height="540"/>
+ </tile>
+ <wangsets>
+  <wangset name="Unnamed Set" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+  </wangset>
+ </wangsets>
 </tileset>
