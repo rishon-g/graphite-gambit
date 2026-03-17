2891015cc5faac648fbba42da32819aaae4e63e1
Napv2103 <krasnenskyalexey@gmail.com>
2026-03-16 23:14:51 -0700

    JavaDocs

--- src/main/java/Entities/ExitPoint.java
+++ src/main/java/Entities/ExitPoint.java
@@ -2,37 +2,64 @@ package Entities;
 
 import Game.GameWorld;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+/**
+ * Non-player entity representing the level exit.
+ *
+ * <p>This entity is rendered as a static trophy sprite in the world. When the
+ * player collides with it, the game win condition is triggered.</p>
+ */
 public class ExitPoint extends Nonplayer {
-    private Texture texture;
 
+    /**
+     * Texture used to render the exit point.
+     */
+    private final Texture texture;
+
+    /**
+     * Creates a new exit point.
+     *
+     * @param world the game world
+     */
     public ExitPoint(GameWorld world) {
         super(world);
         this.transform.setScale(128, 128);
         this.texture = new Texture(Gdx.files.internal("sprites/trophy.png"));
     }
 
+    /**
+     * Updates the internal state of the exit point.
+     *
+     * <p>This entity is static, no update logic is required.</p>
+     *
+     * @param delta time since last update
+     */
     @Override
     public void updateInternal(float delta) { }
 
+    /**
+     * Renders the exit point.
+     *
+     * @param batch sprite batch used for drawing
+     * @param delta time since last update
+     */
     @Override
     public void render(SpriteBatch batch, float delta) {
-        batch.setColor(Color.WHITE);
-        batch.draw(texture,
-                this.transform.position.x * Game.GdxGame.UNIT_SCALE,
-                this.transform.position.y * Game.GdxGame.UNIT_SCALE,
-                this.transform.size.x * Game.GdxGame.UNIT_SCALE,
-                this.transform.size.y * Game.GdxGame.UNIT_SCALE
-        );
+        renderTexture(batch, texture);
     }
 
+    /**
+     * Handles collision between the player and the exit point.
+     *
+     * <p>When the player reaches this object, the win condition is triggered
+     * and this entity is marked as dead.</p>
+     *
+     * @param player the player colliding with the exit point
+     */
     @Override
     public void playerCollide(Player player) {
-        // Trigger the win condition!
         world.winGame();
         this.dead = true;
     }
