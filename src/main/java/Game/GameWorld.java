6779a2903697717e22b657b8e634732109e859a8
Lane Jacobson <lanejacobson2@gmail.com>
2026-03-30 12:26:26 -0700

    Moved GameWorld testing logic into a new rebased branch without global refactoring

--- src/main/java/Game/GameWorld.java
+++ src/main/java/Game/GameWorld.java
@@ -508,12 +508,12 @@ public class GameWorld {
         // grid boundary check, so we cant traverse outside of the map
         if (t.position.x + dx < 0)
             dx = -t.position.x;
-        if (t.position.x + t.size.x + dx > tilemap.length * TILE_SIZE)
-            dx = (tilemap.length * TILE_SIZE) - (t.position.x + t.size.x);
+        if (t.position.x + t.size.x + dx > height)
+            dx = (height) - (t.position.x + t.size.x);
         if (t.position.y + dy < 0)
             dy = -t.position.y;
-        if (t.position.y + t.size.y + dy > tilemap[0].length * TILE_SIZE)
-            dy = (tilemap[0].length * TILE_SIZE) - (t.position.y + t.size.y);
+        if (t.position.y + t.size.y + dy > width)
+            dy = (width) - (t.position.y + t.size.y);
 
         // phantom box to test movements before actually moving the player
         Transform testBox = new Transform();
