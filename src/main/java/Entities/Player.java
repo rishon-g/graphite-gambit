ae5e88bb520d944a4e73bbbd17b2194820e6c56e
Lane Jacobson <lanejacobson2@gmail.com>
2026-04-01 14:25:26 -0700

    Additional unused imports and vars

--- src/main/java/Entities/Player.java
+++ src/main/java/Entities/Player.java
@@ -1,13 +1,10 @@
 package Entities;
-import Game.AudioManager;
 import Game.DrawWeight;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
